4a78415fc1d6b883f6d414500d3ddd29be59d340
alexander yaremchuk <alexander.yaremchuk@gmail.com>
2024-08-29 13:25:04 +0300

    fix: remove std.debug usage from zig code

--- zig/index.zig
+++ zig/index.zig
@@ -4,9 +4,9 @@ export fn add(a: i32, b: i32) i32 {
     return a ^ b;
 }
 
-pub fn main() void {
-    std.debug.print("result is {}", .{add(5, 7)});
-}
+// pub fn main() void {
+//     std.debug.print("result is {}", .{add(5, 7)});
+// }
 
 test "simple addition test" {
     try std.testing.expect(add(3, 2) == 5);
