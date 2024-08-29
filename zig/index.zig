42fad18646b7e80b64f9ea389e76df64033692cf
alexander yaremchuk <alexander.yaremchuk@gmail.com>
2024-08-29 11:24:54 +0300

    feat: Included Zig code, compiling to WebAssebmbly and used on html page.

---
+++ zig/index.zig
@@ -0,0 +1,13 @@
+// const std = @import("std");
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+// pub fn main() void {
+//     // std.debug.print("hello, {}", .{add(1, 2)});
+// }
+
+// test "simple addition test" {
+//     try std.testing.expect(try add(1, 2) == 3);
+// }
