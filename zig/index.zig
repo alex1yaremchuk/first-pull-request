067c452c6e7e3ed9b3ff41d9beb09e56ccbb40af
alexander yaremchuk <alexander.yaremchuk@gmail.com>
2024-08-29 13:53:59 +0300

    fix: change to trigger build

--- zig/index.zig
+++ zig/index.zig
@@ -4,10 +4,10 @@ export fn add(a: i32, b: i32) i32 {
     return a ^ b;
 }
 
-// pub fn main() void {
-//     std.debug.print("result is {}", .{add(5, 7)});
-// }
-
 test "simple addition test" {
     try std.testing.expect(add(3, 2) == 5);
 }
+
+// pub fn main() void {
+//     std.debug.print("result is {}", .{add(5, 7)});
+// }
