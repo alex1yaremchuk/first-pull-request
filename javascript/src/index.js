7738bc4f1ea25ea3a0d9d7b6471a1f8b76afc718
alexander yaremchuk <alexander.yaremchuk@gmail.com>
2024-08-30 12:52:56 +0300

    fix: fix js calc

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,8 +1,8 @@
 export function add(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number')
-    throw new Error('Both arguments should be of type "number"')
+  if (typeof a !== "number" || typeof b !== "number")
+    throw new Error('Both arguments should be of type "number"');
 
-  return a;
+  return a + b;
 }
 
 console.log(
