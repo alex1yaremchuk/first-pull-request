df26a9d5cdc6458838e870fb399a26a63682bf22
alexander yaremchuk <alexander.yaremchuk@gmail.com>
2025-06-27 23:17:46 +0300

    feat: preparing ci/cd changes

---
+++ javascript/tests/js.add.commutative.test.js
@@ -0,0 +1,18 @@
+import fc from 'fast-check';
+import { describe, it, expect } from 'vitest';
+import { add } from '../src/index.js';
+
+describe('add(a, b)', () => {
+  it('must be commutative', () => {
+    fc.assert(
+      fc.property(
+        fc.integer({ min: -1e6, max: 1e6 }),
+        fc.integer({ min: -1e6, max: 1e6 }),
+        (a, b) => add(a, b) === add(b, a)
+      ),
+      {
+        numRuns: 1000,
+      }
+    );
+  });
+});
