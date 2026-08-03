cb6c00bed7464cf4913e5a1dd237ac59c73ba2cb
Alexander Yaremchuk <alexander.yaremchuk@gmail.com>
2026-08-03 15:17:22 +0300

    chore: update zig build script

--- build.zig
+++ build.zig
@@ -10,9 +10,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "index",
-        .root_source_file = b.path("zig/index.zig"),
-        .target = target,
-        .optimize = .ReleaseSmall,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("zig/index.zig"),
+            .target = target,
+            .optimize = .ReleaseSmall,
+        }),
     });
 
     // <https://github.com/ziglang/zig/issues/8633>
